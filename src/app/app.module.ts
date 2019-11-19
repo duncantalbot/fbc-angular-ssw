e66d73a5dec1e70d064e3ad474193e7c11f84caf
Duncan Talbot <duncan_talbot@hotmail.com>
2019-11-19 16:42:27 +1100

    404 Not Found

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,10 @@ import { AppComponent } from './app.component';
 import { CompanyListComponent } from './company/company-list/company-list.component';
 import { CompanyTableComponent } from './company/company-table/company-table.component';
 import { CompanyEditComponent } from './company/company-edit/company-edit.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
-  declarations: [AppComponent, CompanyListComponent, CompanyTableComponent, CompanyEditComponent],
+  declarations: [AppComponent, CompanyListComponent, CompanyTableComponent, CompanyEditComponent, NotFoundComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
