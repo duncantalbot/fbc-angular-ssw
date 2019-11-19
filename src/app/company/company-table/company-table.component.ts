18b32f4200ffb8615faf5074c3d6d72b97580763
Duncan Talbot <duncan_talbot@hotmail.com>
2019-11-19 16:15:52 +1100

    Dumb Components communication

--- src/app/company/company-table/company-table.component.ts
+++ src/app/company/company-table/company-table.component.ts
@@ -1,18 +1,31 @@
-import { Component, OnInit, Input, Output } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  ChangeDetectionStrategy
+} from '@angular/core';
 import { Company } from '../company';
 
 @Component({
   selector: 'fbc-company-table',
   templateUrl: './company-table.component.html',
-  styleUrls: ['./company-table.component.scss']
+  styleUrls: ['./company-table.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CompanyTableComponent implements OnInit {
   @Input()
   companies: Company[];
 
+  @Output()
+  deleteCompanyClicked: EventEmitter<Company> = new EventEmitter<Company>();
+
   constructor() {}
 
   ngOnInit() {}
 
-  deleteCompany() {}
+  deleteCompany(company: Company) {
+    this.deleteCompanyClicked.emit(company);
+  }
 }
